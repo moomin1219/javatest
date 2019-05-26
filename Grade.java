aa3034593057574e44664b8d893e16fd154d25da
moomin1219 <50163277+moomin1219@users.noreply.github.com>
2019-05-26 14:19:06 +0900

    Create Grade.java

---
+++ Grade.java
@@ -0,0 +1,43 @@
+package comparator;
+
+public class Grade {
+
+	private String name;
+	private int korean;
+	private int english;
+	private int math;
+	
+	
+	public Grade(String name, int korean, int english, int math) {
+		super();
+		this.name = name;
+		this.korean = korean;
+		this.english = english;
+		this.math = math;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getKorean() {
+		return korean;
+	}
+	public void setKorean(int korean) {
+		this.korean = korean;
+	}
+	public int getEnglish() {
+		return english;
+	}
+	public void setEnglish(int english) {
+		this.english = english;
+	}
+	public int getMath() {
+		return math;
+	}
+	public void setMath(int math) {
+		this.math = math;
+	}
+	
+}
