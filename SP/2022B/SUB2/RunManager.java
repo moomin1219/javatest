516ceab0469dd49ac8a377c1d162371eb74a7a74
moomin1219 <50163277+moomin1219@users.noreply.github.com>
2023-06-16 01:15:12 +0900

    Create RunManager.java

---
+++ SP/2022B/SUB2/RunManager.java
@@ -0,0 +1,45 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
+public class RunManager {
+
+	public static void main(String[] args) throws IOException {
+		
+		// Worker 생성 및 실행 Sample  - 아래 2개 라인을 지우고 구현하세요.
+		// Worker worker = new Worker(0);
+		// worker.run("VIEW_AD1");
+		
+		Map<Integer, Worker> workerMap = new HashMap<Integer, Worker>();
+		
+		for (int i=0; i < 2; i++) {
+			Worker worker = new Worker(i);
+			workerMap.put(i, worker);
+		}
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String input;
+		
+		while(true) {
+			input = br.readLine();
+			
+			long timestamp = Long.parseLong(input.split(" ")[0]);
+			String queueNum = input.split(" ")[1];
+			String value = input.split(" ")[2];
+			
+			String result = null;
+			
+			Worker worker = workerMap.get(Integer.parseInt(queueNum));
+			
+			result = worker.run(timestamp, value);
+			
+			if(result != null) {
+				System.out.println(result);
+			}
+		}
+	}
+
+}
