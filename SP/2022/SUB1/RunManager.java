7a8233f27caaac2b9bbf96aa72bb09c31f55dd6e
moomin1219 <50163277+moomin1219@users.noreply.github.com>
2023-06-18 01:41:00 +0900

    Update RunManager.java

--- SP/2022/SUB1/RunManager.java
+++ SP/2022/SUB1/RunManager.java
@@ -1,1 +1,34 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class RunManager {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String input;
+		
+		while (true) {
+			
+			input = br.readLine();
+			
+			BufferedReader reader = new BufferedReader(new FileReader("./" + input + ".txt"));
+			
+			String serviceFileName = reader.readLine();
+			
+			BufferedReader reader2 =  new BufferedReader(new FileReader("./" + serviceFileName + ".txt"));
+			
+			System.out.println(reader2.readLine());
+			
+			reader.close();
+			reader2.close();
+			
+			break;
+		}
+
+	}
+
+}
