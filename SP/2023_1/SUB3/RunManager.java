c96f7b611be7d039cf74950e03bae0dade6dd549
moomin1219 <50163277+moomin1219@users.noreply.github.com>
2024-05-12 01:41:19 +0900

    Create RunManager.java

---
+++ SP/2023_1/SUB3/RunManager.java
@@ -0,0 +1,29 @@
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.ServerConnector;
+import org.eclipse.jetty.servlet.ServletHandler;
+
+
+public class RunManager {
+
+	public static void main(String[] args) {
+		
+		Server server = new Server();
+		ServerConnector httpConnector = new ServerConnector(server);
+		httpConnector.setHost("127.0.0.1");
+		httpConnector.setPort(8010);
+		server.addConnector(httpConnector);
+		
+		ServletHandler servletHandler = new ServletHandler();
+		servletHandler.addServletWithMapping(MyServlet.class, "/*");
+		server.setHandler(servletHandler);
+		
+		try {
+			server.start();
+			server.join();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
