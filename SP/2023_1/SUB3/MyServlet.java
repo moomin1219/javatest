5952a218e26f59b1844f50794496f310fd3c5801
moomin1219 <50163277+moomin1219@users.noreply.github.com>
2024-05-12 01:43:07 +0900

    Create MyServlet.java

---
+++ SP/2023_1/SUB3/MyServlet.java
@@ -0,0 +1,25 @@
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.eclipse.jetty.client.HttpClient;
+import org.eclipse.jetty.client.api.ContentResponse;
+import org.eclipse.jetty.http.HttpField;
+import org.eclipse.jetty.http.HttpFields;
+import org.eclipse.jetty.http.HttpMethod;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+public class MyServlet extends HttpServlet {
+
+
+}
